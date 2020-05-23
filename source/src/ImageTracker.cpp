9962af38a42cd6be36a8a2136b06bd0de215b5c7
arming <arming@student.chalmers.se>
2020-05-23 18:33:50 +0200

    Refactor ImageTracker
    Remove unused functions and templateImage
    Fix warnings for not initialized parameters
    Comment functionalities
    
    Co-authored-by: konrado <konrado@student.chalmers.se>

--- source/src/ImageTracker.cpp
+++ source/src/ImageTracker.cpp
@@ -3,22 +3,9 @@
 using namespace cv;
 using namespace std;
 
-ImageTracker::ImageTracker(const String templatePath, const int detectionMethod_, const int minRectArea_, const int maxRectArea_) {
-    setTemplateImage(templatePath);
-    setMethod(detectionMethod_);
-    setMinRectArea(minRectArea_);
-    setMaxRectArea(maxRectArea_);
-}
-
-void ImageTracker::setTemplateImage(const String templatePath) {
-    detectionTemplate = cv::imread(templatePath, 0);
-}
-
-void ImageTracker::matchAndNormalize(const Mat &inputImage, Mat &outputImage) {
-    cv::matchTemplate(inputImage, detectionTemplate, outputImage, detectionMethod);
-    cv::normalize(outputImage, outputImage, 0, 1, NORM_MINMAX, -1, Mat() );
-
-    outputImage.convertTo(outputImage, CV_8UC1);
+ImageTracker::ImageTracker(const int minRectArea_, const int maxRectArea_) {
+    setMinRectArea(minRectArea_); // Set the minimum area of a cone
+    setMaxRectArea(maxRectArea_); // Set the maximum area of a cone
 }
 
 vector<Rect> ImageTracker::detectMatches(const Mat &image, Mat &detections) {
@@ -54,13 +41,13 @@ void ImageTracker::recursiveMerge(vector<Rect> &rectangles) {
     vector<Rect> mergedRectangles;
     vector<bool> usedRectangles(rectangles.size(), false);
     bool mergedSomething = false;
-    for (int i = 0; i < rectangles.size()-1; ++i) {
+    for (size_t i = 0; i < rectangles.size()-1; ++i) {
         if (usedRectangles[i]) {
             continue;
         }
         vector<Rect> overlappingRects;
         Rect unionRects = rectangles[i];
-        for (int j = i+1; j < rectangles.size(); j++) {
+        for (size_t j = i+1; j < rectangles.size(); j++) {
             Rect tempRect = rectangles[j];
             if ((unionRects & tempRect).area() > 0) {
                 unionRects = unionRects | tempRect;
@@ -117,43 +104,6 @@ void ImageTracker::run(const Mat &image, vector<Rect> &rectangles) {
     filterRectsByDimensions(matches);
 
     rectangles = matches;
-}
-
-void ImageTracker::findObjectLocation(const Mat &image, Point &bestMatch) {
-    cv::Mat detections;
-    cv::matchTemplate(image, detectionTemplate, detections, detectionMethod);
-    cv::normalize(detections, detections, 0, 1, NORM_MINMAX, -1, Mat() );
-
-    double minVal;      
-    double maxVal; 
-    cv::Point minLoc;
-    cv::Point maxLoc;
-
-    // localize the detection with the highest confidence
-    minMaxLoc(detections, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-        
-    if (highValuesMatchBest) {
-        bestMatch = maxLoc;
-    } else {
-        bestMatch = minLoc;
-    }
-}
-
-void ImageTracker::setMethod(const int detectionMethod_) {
-    detectionMethod = detectionMethod_; 
-    if (detectionMethod == CV_DIFF || detectionMethod == CV_TM_SQDIFF_NORMED) {
-        highValuesMatchBest = false;
-    } else {
-        highValuesMatchBest = true;
-    }
-}
-
-int ImageTracker::getTemplateWidth() {
-    return detectionTemplate.cols;
-}
-
-int ImageTracker::getTemplateHeight() {
-    return detectionTemplate.rows;
 }
 
 void ImageTracker::setMinRectArea(const int minRectArea_) {
--- source/src/ImageTracker.hpp
+++ source/src/ImageTracker.hpp
@@ -9,33 +9,80 @@
 #include <opencv2/objdetect.hpp>
 #include <iostream>
 
-using namespace cv;
-using namespace std;
-
 class ImageTracker {
     public:
-        ImageTracker(const String templatePath, const int detectionMethod_, const int minRectArea_=100, const int maxRectArea_=5000);
-        void setTemplateImage(const String templatePath);
-        void matchAndNormalize(const Mat& inputImage, Mat &outputImage);
-        vector<Rect> detectMatches(const Mat &image, Mat &detections);
-        void findObjectLocation(const Mat &image, Point &bestMatch);
-        void recursiveMerge(vector<Rect> &rectangles);
-        void filterRectsByArea(vector<Rect> &rectangles, int minArea, int maxArea);
-        void filterRectsByDimensions(vector<Rect> &rectangles, const double widthToHeightRatio=1.0);
-        void mergeOverlappingRectangles(vector<Rect> &rectangles);
-        void run(const Mat &image, vector<Rect> &rectangles);
-        void setMethod(const int detectionMethod_);
-        int getTemplateWidth();
-        int getTemplateHeight();
-        void setMinRectArea(const int minRectArea_);
-        void setMaxRectArea(const int maxRectArea_);
+        /**the rectangeles
+         * Create instance of ImageTracker to detect cones 
+         * 
+         * @param minRectArea_ default minRectArea for this ImageTracker
+         * @param maxRectArea_ default maxRectArea for this ImageTracker 
+         */
+        ImageTracker(const int minRectArea_=100, const int maxRectArea_=5000);
         
+        /**
+         * Detect contours of possible objects in an Image
+         * 
+         * @param image[in] The Image detections are made on
+         * @param detections[out] Return the original image
+         * @return Vector containing rectangular regions 
+         */
+        std::vector<cv::Rect> detectMatches(const cv::Mat &image, cv::Mat &detections);
+
+        /**
+         * Recursively merge overlapping rectangles until no more rectangles overlap
+         * 
+         * @param rectangles Rectangles to be merged (in and out)
+         */
+        void recursiveMerge(std::vector<cv::Rect> &rectangles);
+       
+        /**
+         * Filter out all rectangles based on their area
+         * 
+         * @param rectangles are the rectangles to be filtered
+         * @param minArea is the minimum area a rectangle may be to be considered a cone
+         * @param maxArea is the maximum size a rectangle may be to be considered a cone
+         */
+        void filterRectsByArea(std::vector<cv::Rect> &rectangles, int minArea, int maxArea);
+
+        /**
+         * Filter out all rectangles that don't come in the desired widthToHeightRatio
+         * 
+         * @param rectangles Rectangles that need to be filtered (also used to return the result)
+         * @param widthToHeightRatio Maximum ratio width/height that passes the filter (default=1.0)
+         */
+        void filterRectsByDimensions(std::vector<cv::Rect> &rectangles, const double widthToHeightRatio=1.0);
+        
+        /**
+         * Merge overlapping Rectangles into a single rectangle.
+         * 
+         * @param rectangles Unmerged rectangles that are merged (and returned in the same vector)
+         */
+        void mergeOverlappingRectangles(std::vector<cv::Rect> &rectangles);
+        
+        /**
+         * Run the ImageTracker (actually detector) on a given image and return rectangles for detections
+         * 
+         * @param[in] image Address of the image we want to detect cones on
+         * @param[out] rectangles Vector of rectangles marking cones in the image
+         */
+        void run(const cv::Mat &image, std::vector<cv::Rect> &rectangles);
+
+        /**
+         * Set the minimum area that detection rectangles need have to be considered cones
+         * 
+         * @param minRectArea_ new value to be set for minRectArea
+         */
+        void setMinRectArea(const int minRectArea_);
+
+        /**
+         * Set the maximum area that detection rectangles are allowed to have to be considered cones
+         * 
+         * @param maxRectArea_ new value to be set for maxRectArea
+         */
+        void setMaxRectArea(const int maxRectArea_);        
 
     private:
-        cv::Mat detectionTemplate;
-        int detectionMethod;
-        int minRectArea;
-        int maxRectArea;
-        bool highValuesMatchBest;
+        int minRectArea = 100; // Min area to be classified as a cone
+        int maxRectArea = 5000; // Max area to be classified as a cone
 };
 #endif
