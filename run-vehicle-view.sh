318f0e5d14f75c766bbdec5141c26290f1cdee52
Konrad Otto <konrad-otto@gmx.de>
2020-05-28 12:25:52 +0200

    Fix path to recordings

--- run-vehicle-view.sh
+++ run-vehicle-view.sh
@@ -6,7 +6,7 @@ handle_exit() {
 trap "exit" INT TERM ERR
 trap handle_exit EXIT
 
-cd ../individual/recordings
+cd ./recordings
 docker run --rm --init --net=host --name=vehicle-view -v $PWD:/opt/vehicle-view/recordings -v /var/run/docker.sock:/var/run/docker.sock -p 8081:8081 chalmersrevere/opendlv-vehicle-view-multi:v0.0.60 &
 sleep 2
 firefox localhost:8081/recordings
