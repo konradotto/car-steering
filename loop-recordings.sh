c706e58c017f15900376243462e41fb68423db03
Konrad Otto <konrad-otto@gmx.de>
2020-05-29 01:54:26 +0200

    Reinsert functionalities

--- loop-recordings.sh
+++ loop-recordings.sh
@@ -4,9 +4,11 @@ END=5
 
 for i in $(seq 1 $END); 
     do ./run.sh ${i} || true;
-    # python3 analyser/src/mergeCSV.py csv/report${i}.csv unzipped/csv/report${i}.csv;
-    # cat mergedReports.csv;
-    # python3 analyser/src/analyser.py < mergedReports.csv;
-    # mv performance.png "performancePlot${NR}.png";
-    # rm mergedReports.csv
+    python3 analyser/src/mergeCSV.py csv/report${i}.csv unzipped/csv/report${i}.csv;
+    cat mergedReports.csv;
+    python3 analyser/src/analyser.py < mergedReports.csv;
+    mv performance.png "charts/performancePlot${NR}.png";
+    rm mergedReports.csv
+    docker kill vehicle-view
+    sleep 5
 done
