9bb32ab06d706c79027b9390abb45df8daba15da
Konrad Otto <konrad-otto@gmx.de>
2020-05-28 16:55:13 +0200

    Remove trap

--- run-group13.sh
+++ run-group13.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-handle_exit() {
-	docker kill group13
-	kill -9 %1
-}
 trap "exit" INT TERM ERR
-trap handle_exit EXIT
+trap "kill 0" EXIT
 
 xhost +
 #docker run --rm --net=host --name=group13 --ipc=host -e DISPLAY=$DISPLAY -v /tmp:/tmp -v $PWD/img:/img -v $PWD/csv:/csv/ group13:latest --cid=253 --name=img --width=640 --height=480 --verbose &
