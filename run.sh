be7f497eeafd6da6e1335b66fde6fea0538a6465
Konrad Otto <konrad-otto@gmx.de>
2020-05-28 20:29:39 +0200

    Do not kill

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 #trap "exit" INT TERM ERR
 #trap "kill -SIGINT" EXIT
 
-./run-vehicle-view.sh & ./run-decoder.sh & sleep 10 && docker run -i --rm --net=host sele 2 & sleep 5 && ./run-group13.sh & sleep 20 && kill $! &
+./run-vehicle-view.sh & ./run-decoder.sh & sleep 10 && docker run -i --rm --net=host sele 2 & sleep 5 && ./run-group13.sh & sleep 20 && kill $!
 
 kill 0
