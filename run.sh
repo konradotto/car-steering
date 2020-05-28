b303c56f754829e95f94f7a82ac39acb0ff2baf9
Konrad Otto <konrad-otto@gmx.de>
2020-05-28 20:37:40 +0200

    trap exit

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #trap "exit" INT TERM ERR
-#trap "kill -SIGINT" EXIT
+trap "exit 0" EXIT
 
-./run-vehicle-view.sh & ./run-decoder.sh & sleep 10 && docker run -i --rm --net=host sele 2 & sleep 5 && ./run-group13.sh & sleep 20 && kill -SIGSTOP
+./run-vehicle-view.sh & ./run-decoder.sh & sleep 10 && docker run -i --rm --net=host sele 2 & sleep 5 && ./run-group13.sh & sleep 20 && kill $!
