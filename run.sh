4551d1e4d986546b7de64f1962db33bf8213bf55
Konrad Otto <konrad-otto@gmx.de>
2020-05-28 20:02:56 +0200

    Revert

--- run.sh
+++ run.sh
@@ -4,11 +4,9 @@ trap "kill SIGINT" EXIT
 
 ./run-vehicle-view.sh &
 ./run-decoder.sh &
-sleep 3
+sleep 10
 docker run -i --rm --net=host sele 2
-sleep 3
+sleep 5
 ./run-group13.sh &
 
 sleep 20
-
-kill SIGINT
