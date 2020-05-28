7cbb7e5e86acfc736295e28761fd85abe85a3452
Konrad Otto <konrad-otto@gmx.de>
2020-05-28 20:35:37 +0200

    Use SIGSTOP

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #trap "exit" INT TERM ERR
 #trap "kill -SIGINT" EXIT
 
-./run-vehicle-view.sh & ./run-decoder.sh & sleep 10 && docker run -i --rm --net=host sele 2 & sleep 5 && ./run-group13.sh & sleep 20 && kill -SIGSTOP
+./run-vehicle-view.sh & ./run-decoder.sh & sleep 10 && docker run -i --rm --net=host sele 2 & sleep 5 && ./run-group13.sh & sleep 20 && exit -SIGSTOP
