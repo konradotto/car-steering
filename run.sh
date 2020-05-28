6711f7fd558400f24045133479ac0978c57fdaa4
Konrad Otto <konrad-otto@gmx.de>
2020-05-28 20:45:04 +0200

    Kill in after_script

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #trap "exit" INT TERM ERR
-trap "exit 0" EXIT
 
-./run-vehicle-view.sh & ./run-decoder.sh & sleep 10 && docker run -i --rm --net=host sele 2 & sleep 5 && ./run-group13.sh & sleep 20 && kill $!
+./run-vehicle-view.sh & 
+./run-decoder.sh & 
+sleep 10 
+docker run -i --rm --net=host sele 1
+sleep 5 
+./run-group13.sh & 
