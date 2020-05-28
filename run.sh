556ba9f68db329126c407856368425531f3681dd
Konrad Otto <konrad-otto@gmx.de>
2020-05-29 01:20:51 +0200

    Save csv with recording number

--- run.sh
+++ run.sh
@@ -10,7 +10,8 @@ sleep 5
 docker run -i --rm --net=host sele $selection
 sleep 5 
 ./run-group13.sh & 
-sleep 90
+sleep 20
+mv csv/recordings.csv csv/recording${selection}.png
 
 pkill -f ./run-group13.sh
 pkill -f ./run-decoder.sh
