cfa30c23ea9800f8d5f9f21605fbd11be6821d29
Konrad Otto <konrad-otto@gmx.de>
2020-05-28 16:38:45 +0200

    Kill run from performance job

--- run-group13.sh
+++ run-group13.sh
@@ -7,7 +7,7 @@ trap "exit" INT TERM ERR
 trap handle_exit EXIT
 
 xhost +
-docker run --rm --net=host --name=group13 --ipc=host -e DISPLAY=$DISPLAY -v /tmp:/tmp -v $PWD/img:/img -v $PWD/csv:/csv/ group13:latest --cid=253 --name=img --width=640 --height=480 --verbose &
-#docker run --rm --net=host --name=group13 --ipc=host -v /tmp:/tmp -v $PWD/img:/img -v $PWD/csv:/csv/ group13:latest --cid=253 --name=img --width=640 --height=480 &
+#docker run --rm --net=host --name=group13 --ipc=host -e DISPLAY=$DISPLAY -v /tmp:/tmp -v $PWD/img:/img -v $PWD/csv:/csv/ group13:latest --cid=253 --name=img --width=640 --height=480 --verbose &
+docker run --rm --net=host --name=group13 --ipc=host -v /tmp:/tmp -v $PWD/img:/img -v $PWD/csv:/csv/ group13:latest --cid=253 --name=img --width=640 --height=480 &
 
 wait
