2a3fff4619b9e8e8f5a81c6ee5a0cedfd8994c4f
Konrad Otto <konrad-otto@gmx.de>
2020-05-29 02:10:05 +0200

    Remove cat

--- loop-recordings.sh
+++ loop-recordings.sh
@@ -5,8 +5,8 @@ END=5
 for i in $(seq 1 $END); 
     do ./run.sh ${i} || true;
     python3 analyser/src/mergeCSV.py csv/report${i}.csv unzipped/csv/report${i}.csv;
-    cat mergedReports.csv;
     python3 analyser/src/analyser.py < mergedReports.csv;
     mv performance.png "charts/performancePlot${i}.png";
     rm mergedReports.csv
+    sleep 1
 done
